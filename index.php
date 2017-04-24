3a74c9e5a5a3aa6de242d26edbc545268544b8bf
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-04-24 16:56:29 -0500

    Extracted <script> from head to .js file

--- index.php
+++ index.php
@@ -137,60 +137,8 @@ function flag($oclc,$stateabb,$worldcatkey)
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script>
-            <?php // TODO: extract to script file ?>
-            var simpleOutput;
-            var detailedOutput;
 
-            function checkFile(file)
-            {
-                <?php // TODO: use jQuery Ajax for clarity ?>
-                var xmlhttp = new XMLHttpRequest();
-                xmlhttp.onreadystatechange = function()
-                {
-                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-                    {
-                        document.getElementById('loadingIcon').classList.add('dNone');
-                        if (xmlhttp.responseText == '-1')
-                        {}
-                        else if (xmlhttp.responseText == "Connection_Error")
-                        {
-                            alert("There were issues either parsing the file or connecting to www.worldcat.org");
-                        }
-                        else
-                        {
-                            document.getElementById('input').classList.add('dNone');
-                            document.getElementById('output').classList.remove('dNone');
-                            var parser = new DOMParser();
-                            var xml = parser.parseFromString(xmlhttp.responseText, "text/xml");
-                            simpleOutput = xml.getElementsByTagName('simpleOutput')[0].childNodes[0].nodeValue;
-                            detailedOutput = xml.getElementsByTagName('detailedOutput')[0].childNodes[0].nodeValue;
-                            document.getElementById('input').classList.add('dNone');
-                            if(xml.getElementsByTagName('liatLibrary')[0].childNodes[0].nodeValue != '')
-                            {
-                                document.getElementById('atLibrary').parentNode.classList.remove('dNone');
-                                document.getElementById('atLibrary').innerHTML = xml.getElementsByTagName('liatLibrary')[0].childNodes[0].nodeValue;
-                            }
-                        }
-                    }
-                };
-                xmlhttp.open("POST", document.location.href, true);
-                var formData = new FormData();
-                formData.append("file-input", file);
-                xmlhttp.send(formData);
-                document.getElementById('loadingIcon').classList.remove('dNone');
-            }
-
-            function downloadSimple()
-            {
-                document.location = 'data:application/octet-stream,' + encodeURIComponent(simpleOutput);
-            }
-
-            function downloadDetailed()
-            {
-                document.location = 'data:application/octet-stream,' + encodeURIComponent(detailedOutput);
-            }
-        </script>
+        <script src="res/js/index.js"></script>
     </head>
 
 <?php
