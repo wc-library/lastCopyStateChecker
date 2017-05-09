6f8b698eac241529917dd378bc675bfd87db36a8
Jeffrey Mudge <jeffmudge@gmail.com>
2017-05-09 15:44:12 -0500

    Update header.php
    make script src protocol aware with //

--- templates/header.php
+++ templates/header.php
@@ -13,8 +13,8 @@
     <?php // stylesheet includes Bootstrap + Bootswatch theme ?>
     <link rel="stylesheet" href="res/css/index.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
     <script src="res/js/index.js"></script>
     <script src="res/js/config.js"></script>
